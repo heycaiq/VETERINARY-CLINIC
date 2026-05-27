ae4a2abeb70c772d76e950d218ce6f54816afde1
heycaiq <ovianacaique@gmail.com>
2026-05-26 21:06:12 -0300

    Refactor Record class to MedicalRecord and update methods

--- src/PETCARE/MODEL/Record.java
+++ src/PETCARE/MODEL/Record.java
@@ -1,42 +1,42 @@
-package PETCARE.MODEL;
+package petcaremodel;
 
 import java.util.List;
 import java.util.ArrayList;
 
 // COMPOSIÇÃO: PRONTUÁRIO SÓ EXISTE VINCULADO A UM ANIMAL
-public class Record {
+public class MedicalRecord {
 
-	private List<Service> history;
+		private List<Service> history;
 
-	public Record() {
-		this.history = new ArrayList<>();
-	}
-
-	// ENCAPSULAMENTO: SÓ ADICIONA ATENDIMENTO POR ESTE MÉTODO CONTROLADO
-	public void addService(Service service) {
-		if (service == null) {
-			throw new IllegalArgumentException("Service cannot be nonexistent!");
+		public MedicalRecord() {
+			this.history = new ArrayList<>();
 		}
-		
-		history.add(service);
-		System.out.println(" ✔ Service recorded in the medical record.");
-	}
 
-	public List<Service> getHistory() {
-		return new ArrayList<>(history);
-	}
-
-	public void displayHistory() {
-		if (history.isEmpty()) {
-			System.out.println(" No service was recorded.");
-			return;
+		// ENCAPSULAMENTO: SÓ ADICIONA ATENDIMENTO POR ESTE MÉTODO CONTROLADO
+		public void addService(Service service) {
+			if (service == null) {
+				throw new IllegalArgumentException("Service cannot be nonexistent!");
+			}
+			
+			history.add(service);
+			System.out.println(" ✔ Service recorded in the medical record.");
 		}
-		for (Service a : history) {
-			System.out.println(" - " + a);
+
+		public List<Service> getHistory() {
+			return new ArrayList<>(history);
+		}
+
+		public void displayHistory() {
+			if (history.isEmpty()) {
+				System.out.println(" No service was recorded.");
+				return;
+			}
+			for (Service a : history) {
+				System.out.println(" - " + a);
+			}
+		}
+
+		public int getTotalService() {
+			return history.size();
 		}
 	}
-
-	public int getTotalService() {
-		return history.size();
-	}
-}
